85977d339f99ecf8ea44a0cdb61b0c201d6eb080
Einar-Eide <einar.eidee@gmail.com>
2026-03-19 16:01:31 +0100

    uml diagram updadet

--- skeleton_project/source/LogicController.c
+++ skeleton_project/source/LogicController.c
@@ -12,14 +12,6 @@ static void doorHandling(int state_enter);
 
 
 
-// tmp
-void delay_ms(int milliseconds) {
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
-}
-
 /*
     #### Static variables ####
 */
